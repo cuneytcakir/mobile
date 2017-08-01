ddc181b117a91afe705369ee154327d10d1ef0cc
cuneytcakir <cuneyt-cakir@yandex.com>
2017-08-01 16:07:01 +0300

    yeni syso lar tanımlandı.

--- src/test/java/iostest/test_ios.java
+++ src/test/java/iostest/test_ios.java
@@ -3,7 +3,10 @@ package iostest;
 public class test_ios {
 	private void syso() {
 		// TODO Auto-generated method stub
-System.out.println("cüneyt çakır");
+System.out.println("cüneyt çakır0");
+System.out.println("cüneyt çakır1");
+System.out.println("cüneyt çakır2");
+System.out.println("cüneyt çakır3");
 	}
 
 }

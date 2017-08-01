ab2eb4591fbb1078c8627aed2b0db956f5638816
cuneytcakir <cuneyt-cakir@yandex.com>
2017-08-01 16:39:08 +0300

    ios - 0 eklendi.

--- src/test/java/iostest/test_ios.java
+++ src/test/java/iostest/test_ios.java
@@ -7,6 +7,7 @@ System.out.println("cüneyt çakır0");
 System.out.println("cüneyt çakır1");
 System.out.println("cüneyt çakır2");
 System.out.println("cüneyt çakır3");
+System.out.println("ios - 0");
 	}
 
 }

cda930fe1559b6b302c17335ae806a39e052bab8
cuneytcakir <cuneyt-cakir@yandex.com>
2017-08-01 16:59:18 +0300

    ondroid - 3'e kadar eklendi.

--- src/test/java/androidtest/testando.java
+++ src/test/java/androidtest/testando.java
@@ -4,6 +4,9 @@ public class testando {
 	private void syso() {
 		// TODO Auto-generated method stub
 		System.out.println("android - 0");
+		System.out.println("android - 1");
+		System.out.println("android - 2");
+		System.out.println("android - 3");
 	}
 
 }
